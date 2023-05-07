e6987432b5bd120d2db0af77d8041dd9f16de002
SukharevaDarina <aug12082018@gmail.com>
2023-05-08 01:19:01 +0300

    workplz

---
+++ Proj.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void read(std::vector<int>& vec) {
+
+	int a;
+	int n;
+	cout << "Enter size: ";
+	cin >> n;
+
+	cout << "Enter "<< n << " numbers: ";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		vec.push_back(a);
+
+	};
+}
+
+	void print(const std::vector<int>&vec) {
+
+		cout << "Vector: ";
+
+		for (auto elem : vec)
+		{
+			cout << elem << ' ';
+
+		}
+	}
+
+int main() {
+	std::vector<int> vec;
+	read(vec);
+	std::sort(vec.begin(), vec.end());
+	print(vec);
+	return 0;
+}
